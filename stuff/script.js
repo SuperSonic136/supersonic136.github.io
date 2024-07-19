cea1998d589aac50f9291970955be02f31c478ae
SuperSonic136 <148922886+SuperSonic136@users.noreply.github.com>
2024-07-19 15:39:32 -0600

    Add files via upload

---
+++ stuff/script.js
@@ -0,0 +1,25 @@
+const navstuffs = [
+    'Home',
+    'Links',
+    'My Work',
+];
+
+const navlinks = [
+    "index.html",
+    "socials.html",
+    "https://www.youtube.com/@supersonic236_",
+];
+
+
+const navbar = document.getElementById('navitem');
+
+function loadnavs(){
+    for (i in navstuffs){
+        const newnavthing = document.createElement('a');
+        newnavthing.href = navlinks[i];
+        newnavthing.textContent = navstuffs[i];
+        listitem = document.createElement('li');
+        listitem.appendChild(newnavthing);
+        document.getElementById('navitem').append(listitem);
+    }
+}
