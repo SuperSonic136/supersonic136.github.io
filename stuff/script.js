97016f5b688abc5c0afa2e47f912049fc19431a4
SuperSonic136 <supersonic1360@gmail.com>
2025-06-06 09:38:48 -0600

    mmm... cookies....

--- stuff/script.js
+++ stuff/script.js
@@ -10,6 +10,29 @@ const navlinks = [
     "/mywork",
 ];
 
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 function addbigassbar(text) {
     
 
@@ -45,15 +68,21 @@ function adddarkmodetoggle() {
     input.id = "themetoggle"
     input.type = "checkbox"
     click.append(input)
-
     var img = document.createElement('img')
 
     var clicked = input.checked ? 1:0
-    img.src = "/stuff/cdn/ico_"+themelist[clicked]+".png"
+    if (getCookie('theme') == null) {
+        img.src = "/stuff/cdn/ico_"+themelist[clicked]+".png"
+    }else {
+        img.src = "/stuff/cdn/ico_"+getCookie('theme')+".png"
+        document.body.style.colorScheme = getCookie('theme')
+    }
+
     input.onclick = function() {
         var clicked = input.checked ? 1:0
         img.src = "/stuff/cdn/ico_"+themelist[clicked]+".png"
         document.body.style.colorScheme = themelist[clicked]
+        setCookie("theme",themelist[clicked],Infinity)
     }
 
     click.append(img)
@@ -61,6 +90,9 @@ function adddarkmodetoggle() {
 }
 
 function loadnavs(){
+    //check dark mode
+    adddarkmodetoggle()
+
     //creates the banner image
     var baner = document.createElement("div")
     baner.className = "baner"
@@ -91,7 +123,7 @@ function loadnavs(){
     document.getElementsByClassName('everything')[0].prepend(baner)
 
     // add dark mode toggle
-    adddarkmodetoggle()
+    
 }
 
  function ahahah() {
